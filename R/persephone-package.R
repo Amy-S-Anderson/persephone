760cf547d521dc5973940d02f72fb5cde53d3385
Nick Gauthier <nicolas.gauthier@ufl.edu>
2026-03-13 15:40:21 -0600

    Setup pkgdown site

--- R/persephone-package.R
+++ R/persephone-package.R
@@ -5,6 +5,16 @@
 #' @importFrom utils write.csv
 "_PACKAGE"
 
+#' Launch the Persephone Shiny app
+#'
+#' @export
+run_app <- function() {
+  if (!requireNamespace("shiny", quietly = TRUE)) {
+    stop("Package 'shiny' is required to run the app. Install it with install.packages('shiny').")
+  }
+  shiny::runApp(system.file("app", package = "persephone"))
+}
+
 # Suppress R CMD check NOTEs for NSE column references used in dplyr/ggplot2
 utils::globalVariables(c(
   "Age_Interval", "CoaleDemenyWestF5", "Lesion_Percent",
